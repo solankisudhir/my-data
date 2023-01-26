fef7e0f7ed10226eb3792a2f23fbf04d479c1a46
solankisudhir <solankisudhir45@gmail.com>
2023-01-26 04:37:11 -0800

    javascript

---
+++ selctor.js
@@ -0,0 +1,28 @@
+// id slector:
+// let x = document.getElementById('h-1')
+// let x = document.getElementById('per-1').innerText = 'hello'
+// let x = document.getElementById('per-3').innerHTML = 'hello'
+// let x = document.getElementById('per-3').style.color ='hotpink'
+
+    // x = document.childNodes
+    // x = document.parentNode
+
+// console.log(x);
+
+// classname selector:
+// let y = document.getElementsByClassName('h-2')
+// let y = document.getElementsByClassName('per')
+    // y = document.childNodes
+    // y = document.parentNode
+
+// console.log(y);
+
+// queryselector:
+// let z = document.querySelector('.per')
+//     z = document.querySelector('#heading')
+//     z.style.color = 'red'
+// console.log(z);
+
+// Nameslector:
+let a = document.getElementsByName('hello')
+console.log(a);
