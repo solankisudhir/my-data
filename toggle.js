15f46ca6d5180f113ac5be65e5697af64b4cd133
solankisudhir <solankisudhir45@gmail.com>
2023-02-22 04:09:59 -0800

    jquery

---
+++ toggle.js
@@ -0,0 +1,9 @@
+$('#add').click(function () {
+    $('#box').addClass('bg color')
+})
+$('#remove').click(function () {
+    $('#box').removeClass('bg color')
+})
+$('#toggle').click(function () {
+    $('#box').toggleClass('bg color')
+})
