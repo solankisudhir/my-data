f349784efbc93618961b447e9bccd9c0faab5203
solankisudhir <solankisudhir45@gmail.com>
2023-03-16 05:43:42 -0700

    array-method

---
+++ sort-reverse.js
@@ -0,0 +1,10 @@
+let a = ["sudhir", "zankan", "vimal"]
+console.log(a);
+a.sort();
+console.log(a);
+a.reverse();
+console.log(a);
+a.shift();
+console.log(a);
+a.unshift("naresh");
+console.log(a);
